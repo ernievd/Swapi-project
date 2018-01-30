71277730bd89dbe76fa7c5fdf83eec84de73d0c9
ernievd <ernievd@gmail.com>
2018-01-30 08:27:02 -0600

    updated server.js to use remote database

--- server/server.js
+++ server/server.js
@@ -16,8 +16,23 @@ app.use('/favorites', favoritesRouter);
 /** ---------- MONGOOSE ------------ **/
 const mongoose = require('mongoose');
 
-// 27017 is the default mongo port number
+
+
+// // 27017 is the default mongo port number
 const databaseUrl = 'mongodb://localhost:27017/swapicollection';
+
+// Mongo Connection //
+var mongoURI = '';
+// process.env.MONGODB_URI will only be defined if you
+// are running on Heroku
+if(process.env.MONGODB_URI != undefined) {
+	// use the string value of the environment variable
+	mongoURI = process.env.MONGODB_URI;
+} else {
+	// use the local database server
+	mongoURI = 'mongodb://localhost:27017/databasename';
+}
+
 
 // connect to mongoDB
 mongoose.connect(databaseUrl);
